55a1e3278f9cea545bad96118c7a03f339e63b41
tiluong1 <146888143+tiluong1@users.noreply.github.com>
2023-11-08 15:10:52 -0800

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -8,6 +8,32 @@ mkdir grading-area
 git clone $1 student-submission
 echo 'Finished cloning'
 
+files=`find test-files`
+PASSED=0
+FAILED=0
+
+for file in $files
+do
+    # check if a file is a txt file
+    if [[ -f $file && $file == test*.txt ]]
+    then
+        # echo "Currently testing" $file
+        result=`java AgeCalc < $file`
+        # first challenge: print test output 
+        expected=`cat $file.expect`
+        #echo "Expected file" $expect
+        if [[ $result == $expected ]]
+        then
+            echo "Test Passed!"
+            PASSED=$(($PASSED+1))
+        else
+            echo "Test Failed!"
+            FAILED=$(($FAILED+1))
+        fi
+    fi
+done
+
+echo "Your grade is $(($((PASSED*100))/$((PASSED+FAILED))))%"
 
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
